447c3467f8a9f795174f4027af044e96b3796978
JefersonSantosTI <jefersonbrandaosantos@gmail.com>
2026-03-15 11:26:44 -0300

    Initial commit: clean start

--- API/server.js
+++ API/server.js
@@ -1,22 +1,28 @@
-import express from 'express'
-import receitasRoutes from './src/routes/receitas.route.js'
-import cors from 'cors'
-import dotenv from "dotenv"
-dotenv.config()
+import express from "express";
+import receitasRoutes from "./src/routes/receitas.route.js";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-
+dotenv.config();
 
 const app = express();
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
-app.use(cors())
+// Conectar MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("✅ MongoDB conectado"))
+.catch(err => console.error("❌ Erro MongoDB:", err));
 
+app.use(cors());
 app.use(express.json());
 
-app.use('/receitas', receitasRoutes);
+app.use("/receitas", receitasRoutes);
 
-app.listen(PORT, () =>{
-    console.log(`Servidor Rodando na Porta ${PORT}`);
+app.get("/", (req, res) => {
+  res.send("API TreinoFit rodando 🚀");
+});
 
-
-})
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
