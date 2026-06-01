e2cb6d7d65696b29049657e76b2cf38947d72e2f
JefersonSantosTI <jefersonbrandaosantos@gmail.com>
2026-06-01 20:52:04 -0300

    atualizado

--- src/controllers/Personal.js
+++ src/controllers/Personal.js
@@ -1,14 +1,13 @@
+import mongoose from 'mongoose';
+
 const PersonalSchema = new mongoose.Schema({
   nome: { type: String, required: true },
-  // O Mongoose vai buscar o campo "e-mail" no banco, mas usar como "email" no código
-  email: { 
-    type: String, 
-    required: true, 
-    unique: true,
-    map: 'e-mail' 
-  },
+  email: { type: String, required: true, unique: true }, 
   cref: { type: String, required: true, unique: true },
   googleId: { type: String, required: true },
   foto: { type: String },
   assinaturaAtiva: { type: Boolean, default: false }
 }, { timestamps: true });
+
+// Exportação padrão obrigatória
+export default mongoose.model('Personal', PersonalSchema, 'personais');
