f00a3e65f2f8dcb9b15700596f52693e045e193f
JefersonSantosTI <jefersonbrandaosantos@gmail.com>
2026-06-01 19:50:35 -0300

    atualizado

--- src/controllers/Personal.js
+++ src/controllers/Personal.js
@@ -5,7 +5,9 @@ const PersonalSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true }, // O E-mail do Google (único)
   cref: { type: String, required: true, unique: true },  // A licença de trabalho
   googleId: { type: String, required: true },            // O ID de segurança do Google
-  foto: { type: String }                                 // A foto de perfil do Google
+  foto: { type: String },                                // A foto de perfil do Google
+  // ✅ CAMPO OBRIGATÓRIO PARA A INTEGRAÇÃO COM KIWIFY
+  assinaturaAtiva: { type: Boolean, default: false }     // Controla o acesso ao sistema
 }, { timestamps: true });
 
 export default mongoose.model('Personal', PersonalSchema, 'personais');
